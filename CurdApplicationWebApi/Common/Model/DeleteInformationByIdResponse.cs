74eda3a8e3fb23568e68ab1c58c57bdc77b10edc
akashsingh7440 <akashsingh7440@gmail.com>
2024-01-16 20:17:54 +0530

    Delete and Get All Delete User.

---
+++ CurdApplicationWebApi/Common/Model/DeleteInformationByIdResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace CurdApplicationWebApi.Common.Model
+{
+    public class DeleteInformationByIdResponse
+    {
+        public bool IsSuccessfull { get; set; }
+        public string Message { get; set; }
+
+    }
+}
